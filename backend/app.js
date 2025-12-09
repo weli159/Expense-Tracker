369f2b1ff3032dbeeea6c141c4cf1a8b41058832
Olivieryr <olivieryerro97212@gmail.com>
2025-12-09 15:02:09 +0100

    Added CRUD backend features

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import { connectToDb } from "./db/db.js";
 import dotenv from "dotenv";
-
+import transactionRoutes from "./routes/transactions.js"
 // Load config
 dotenv.config();
 
@@ -16,15 +16,15 @@ app.use(cors());
 // Placeholder for Routes (Students B & D will uncomment these)
 import userRoutes from "./routes/userRoute.js";
 // import transactionRoutes from "./routes/transactions.js";
- app.use("/api/auth", userRoutes);
-// app.use("/api/v1", transactionRoutes);
+app.use("/api/auth", userRoutes);
+app.use("/api/v1", transactionRoutes);
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
-    res.status(500).json({ 
-        success: false, 
+    res.status(500).json({
+        success: false,
         message: "Internal Server Error",
-        error: err.message 
+        error: err.message
     });
 });
 
